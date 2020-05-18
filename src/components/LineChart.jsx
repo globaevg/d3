1abc8b0075acc18e4f0808823d71415f2764147f
Yauhen Hloba <globaevg@gmail.com>
2020-05-18 15:22:06 +0300

    completed lineChart

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -10,9 +10,10 @@ import {
   axisRight,
   scaleOrdinal,
   schemeSet1,
+  schemeSet2,
+  schemeSet3,
   line,
   curveCardinal,
-  entries,
 } from 'd3';
 
 const data = [
@@ -156,6 +157,7 @@ export const LineChart = () => {
       .call(yAxis);
 
     const colorScale = scaleOrdinal(schemeSet1);
+    const schemeSet = [...schemeSet1, ...schemeSet2, ...schemeSet3];
 
     const myLine = line()
       .x((value) => xScale(value[0]))
@@ -173,35 +175,47 @@ export const LineChart = () => {
       .enter()
       .append('path')
       .attr('d', (d) => myLine(d))
-      .attr('stroke', colorScale)
+      .attr('stroke', (d, i) => schemeSet[i])
+      .attr('stroke-width', 2)
       .attr('fill', 'none');
 
-    svgLeg
+    svg
+      .selectAll('myDots')
+      .data(lineData)
+      .enter()
+      .append('g')
+      .style('fill', (d, i) => schemeSet[i])
+      .selectAll('myPoints')
+      .data((d) => d)
+      .enter()
       .append('circle')
-      .attr('cx', 50)
-      .attr('cy', 130)
-      .attr('r', 6)
-      .style('fill', '#69b3a2');
+      .attr('cx', (d) => xScale(d[0]))
+      .attr('cy', (d) => dimensions.height - yScale(d[1]))
+      .attr('r', 3);
+
+    // Add one dot in the legend for each name.
     svgLeg
+      .selectAll('legendsDots')
+      .data(subgroups)
+      .enter()
       .append('circle')
-      .attr('cx', 50)
-      .attr('cy', 160)
-      .attr('r', 6)
-      .style('fill', '#404080');
+      .attr('cx', 60)
+      .attr('cy', (d, i) => {
+        return 100 + i * 25;
+      }) // 100 is where the first dot appears. 25 is the distance between dots
+      .attr('r', 7)
+      .style('fill', (d, i) => schemeSet[i]);
+
     svgLeg
+      .selectAll('legendsNames')
+      .data(subgroups)
+      .enter()
       .append('text')
-      .attr('x', 50)
-      .attr('y', 130)
-      .text('variable A')
-      .style('font-size', '15px')
-      .attr('alignment-baseline', 'middle');
-    svgLeg
-      .append('text')
-      .attr('x', 50)
-      .attr('y', 160)
-      .text('variable B')
-      .style('font-size', '15px')
-      .attr('alignment-baseline', 'middle');
+      .attr('x', 80)
+      .attr('y', (d, i) => 100 + i * 25) // 100 is where the first dot appears. 25 is the distance between dots
+      .style('fill', (d, i) => schemeSet[i])
+      .text((d) => d)
+      .style('alignment-baseline', 'middle');
 
     // //
   }, [data, dimensions]);
@@ -228,14 +242,14 @@ export const LineChart = () => {
         <div
           className='line-chart-legend'
           style={{
-            minWidth: '150px',
+            minWidth: '200px',
             height: '400px',
             border: '1px solid green',
           }}
         >
           <svg
             ref={svgLegend}
-            style={{ overflow: 'visible', height: '400px' }}
+            style={{ overflow: 'visible', height: '400px', width: '200px' }}
           ></svg>
         </div>
       </div>
